93a3fafca231fdbe6dc7a176c7be24fed58c824d
LKNorman <lknorman30@gmail.com>
2020-03-31 23:06:30 -0400

    prettier on all files, added function to write html file

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
@@ -16,106 +16,115 @@ var team = [];
 buildTeam();
 
 function buildTeam() {
-    addManager();
-    addEmployees();
+  addManager();
+  addEmployees();
 }
 // function to prompt the information input for the team manager, first because only one per team
 function addManager() {
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "What is the team manager's name?"
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "What is their id?"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is their email address?"
-        },
-        {
-            type: "input",
-            name: "officeNumber",
-            message: "What is the managers office number?"
-        }
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the team manager's name?"
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is their id?"
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is their email address?"
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the managers office number?"
+      }
     ])
-        .then(val => {
-            team.push(new Manager(val.name, val.id, val.email, val.officeNumber));
-        })
-};
+    .then(val => {
+      team.push(new Manager(val.name, val.id, val.email, val.officeNumber));
+    });
+}
 
 // function to build out the team members
 function addEmployees() {
-    return inquirer.prompt([
-        {
-            type: "list",
-            name: "role",
-            message: "What role is this employee?",
-            choices: ["Engineer", "Intern"],
-        },
-        {
-            type: "input",
-            name: "name",
-            message: "What is the employee's name?"
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "What is the employee's ID?",
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is their email address?",
-        },
-        {
-            when: val => val.role === "Engineer",
-            type: "input",
-            name: "github",
-            message: "What is their github username?"
-        },
-        {
-            when: val => val.role === "Intern",
-            type: "input",
-            name: "school",
-            message: "What school do they go to?"
-        }
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "What role is this employee?",
+        choices: ["Engineer", "Intern"]
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?"
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the employee's ID?"
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is their email address?"
+      },
+      {
+        when: val => val.role === "Engineer",
+        type: "input",
+        name: "github",
+        message: "What is their github username?"
+      },
+      {
+        when: val => val.role === "Intern",
+        type: "input",
+        name: "school",
+        message: "What school do they go to?"
+      }
     ])
-        .then(val => {
-            if (val.role === "Engineer") {
-                team.push(new Engineer(val.name, val.id, val.email, val.github));
-            } else {
-                team.push(new Intern(val.name, val.id, val.email, val.school));
-            }
-            // calling the below functions to redo all of the prompts above
-            addAnotherEmployee();
-        })
+    .then(val => {
+      if (val.role === "Engineer") {
+        team.push(new Engineer(val.name, val.id, val.email, val.github));
+      } else {
+        team.push(new Intern(val.name, val.id, val.email, val.school));
+      }
+      // calling the below functions to redo all of the prompts above
+      addAnotherEmployee();
+    });
 }
 // asking if there are any more engineers/interns
 function addAnotherEmployee() {
-    inquire.prompt([
-        {
-            type: "list",
-            name: "newEmployee",
-            message: "Are there any more employees on this team?",
-            choices: ["yes", "no"]
-        }
+  inquire
+    .prompt([
+      {
+        type: "list",
+        name: "newEmployee",
+        message: "Are there any more employees on this team?",
+        choices: ["yes", "no"]
+      }
     ])
-        .then(val => {
-            if (val.addAnotherEmployee === "yes") {
-                this.addEmployees();
-            } else {
-                fs.writeFile(outputPath, render(team), err => {
-                    if (err) {
-                        throw err;
-                    } else {
-                        console.log("Created file", outputPath);
-                    }
-                });
-            }
-        });
+    .then(val => {
+      if (val.addAnotherEmployee === "yes") {
+        this.addEmployees();
+      } else {
+        writeHTML();
+      }
+    });
 }
+
+const writeHTML = () => {
+  if (!fs.existsSync("./ouput")) {
+    fs.mkdirSync("./output");
+  }
+  fs.writeFile(outputPath, render(team), err => {
+    if (err) throw err;
+    console.log("Team.html created");
+  });
+};
+
+buildTeam();
